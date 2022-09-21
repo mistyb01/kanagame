1122231d208ce057456a4f92d42993c537e36a6e
Misty Becker <mistyb01@uw.edu>
2022-09-20 21:27:25 -0700

    cards are randomized!

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 import { kana } from './quizdata.js';
 
+// document selectors
 const cardDisplay = document.querySelector('#card-display');
 const answerInput = document.querySelector('#answer-input');
 answerInput.addEventListener('keydown', checkKey);
 const statusText = document.querySelector('#status');
-
 const startBtn = document.querySelector('#start-button');
 startBtn.addEventListener('click', startQuiz);
 
-
+// default values
 let correctCounter = 0;
 let incorrectCounter = 0;
+let currentIndex = Math.floor(Math.random() * kana.length);
+// get random index: Math.floor(Math.random() * kana.length);
 
-let currentIndex = 0;
+// arrays
+const currentGame = [...kana];
+const correctCards = [];
+const incorrectCards = [];
+
 function startQuiz() {
     startBtn.style.display = 'none';
     document.querySelector('#game').style.display = 'block';
-    cardDisplay.innerText = kana[0].kana;
+    cardDisplay.innerText = currentGame[currentIndex].kana;
     answerInput.focus();
     document.querySelector('#correct').innerText = correctCounter + ' correct';
     document.querySelector('#incorrect').innerText = incorrectCounter + ' incorrect';
@@ -33,15 +39,21 @@ function nextCard() {
 }
 
 function checkAnswer() {
-    if (answerInput.value == kana[currentIndex].romaji) {
+    
+    if (answerInput.value == currentGame[currentIndex].romaji) {
         if (!wrongBefore) {
             correctCounter++;
-        }
-        currentIndex++;
-        if (currentIndex < kana.length) {
-            cardDisplay.innerText = kana[currentIndex].kana;
-            wrongBefore = false;
-        } else {
+        }         
+        //add it to the incorrect/correct pile (depends on if they answered right the first time)
+        wrongBefore ? incorrectCards.push(currentGame[currentIndex]) : correctCards.push(currentGame[currentIndex]);
+        currentGame.splice(currentIndex, 1); //remove the just answered card from the game array
+        
+        if (currentGame.length > 0) {
+        // randomize the current index and go to the next card!
+        currentIndex = Math.floor(Math.random() * currentGame.length);
+        cardDisplay.innerText = currentGame[currentIndex].kana;
+        wrongBefore = false;
+        } else { //game over!
             document.querySelector('#game').style.display = 'none';
             document.querySelector('#game-finish').style.display = 'block';
             const retryBtn = document.querySelector('#retry-button');
@@ -49,9 +61,12 @@ function checkAnswer() {
         }
     } else {
         incorrectCounter++;
-        statusText.innerText = "incorrect, the answer is " + kana[currentIndex].romaji;
+        statusText.innerText = "incorrect, the answer is " + currentGame[currentIndex].romaji;
         wrongBefore = true;
     }
+    
+    console.log("correct", correctCards);
+    console.log("incorrect", incorrectCards);
 }
 
 function checkKey(e) {
