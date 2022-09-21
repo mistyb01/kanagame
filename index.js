9abf56313f2c450c00f8a0846f85ee5d9506cafc
Misty Becker <mistyb01@uw.edu>
2022-09-20 20:52:41 -0700

    added retry button

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { kana } from './quizdata.js';
 
-const startBtn = document.querySelector('#start-button');
-startBtn.addEventListener('click', startQuiz);
-
 const cardDisplay = document.querySelector('#card-display');
 const answerInput = document.querySelector('#answer-input');
 answerInput.addEventListener('keydown', checkKey);
 const statusText = document.querySelector('#status');
+
+const startBtn = document.querySelector('#start-button');
+startBtn.addEventListener('click', startQuiz);
+
 
 let correctCounter = 0;
 let incorrectCounter = 0;
@@ -17,13 +18,21 @@ function startQuiz() {
     document.querySelector('#game').style.display = 'block';
     cardDisplay.innerText = kana[0].kana;
     answerInput.focus();
+    document.querySelector('#correct').innerText = correctCounter + ' correct';
+    document.querySelector('#incorrect').innerText = incorrectCounter + ' incorrect';
 }
 
 let wrongBefore = false;
 function nextCard() {
     answerInput.focus();
     statusText.innerText = "";
+    checkAnswer();
+    answerInput.value = "";
+    document.querySelector('#correct').innerText = correctCounter + ' correct';
+    document.querySelector('#incorrect').innerText = incorrectCounter + ' incorrect';
+}
 
+function checkAnswer() {
     if (answerInput.value == kana[currentIndex].romaji) {
         if (!wrongBefore) {
             correctCounter++;
@@ -35,19 +44,26 @@ function nextCard() {
         } else {
             document.querySelector('#game').style.display = 'none';
             document.querySelector('#game-finish').style.display = 'block';
+            const retryBtn = document.querySelector('#retry-button');
+            retryBtn.addEventListener('click', retryQuiz);
         }
     } else {
         incorrectCounter++;
         statusText.innerText = "incorrect, the answer is " + kana[currentIndex].romaji;
         wrongBefore = true;
     }
-    answerInput.value = "";
-    document.querySelector('#correct').innerText = correctCounter + ' correct';
-    document.querySelector('#incorrect').innerText = incorrectCounter + ' incorrect';
 }
 
 function checkKey(e) {
     if(e && e.keyCode == 13) {
         nextCard();
     }
+}
+
+function retryQuiz() {
+    correctCounter = 0;
+    incorrectCounter = 0;
+    currentIndex = 0;
+    document.querySelector('#game-finish').style.display = 'none';
+    startQuiz();
 }
